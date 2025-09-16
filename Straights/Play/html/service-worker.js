2547499e6942c9474fd8706ad2c6db5c585a1c75
m-ringler <42344272+m-ringler@users.noreply.github.com>
2025-09-17 01:32:03 +0200

    Add some logging to service worker

--- Straights/Play/html/service-worker.js
+++ Straights/Play/html/service-worker.js
@@ -2,7 +2,7 @@
 //
 // SPDX-License-Identifier: GPL-3.0-or-later
 
-const CACHE_NAME = 'v0.6.17-beta4';
+const CACHE_NAME = 'v0.6.17-beta8';
 const urlsToCache = [
   './',
   './favicon.ico',
@@ -18,6 +18,8 @@ const urlsToCache = [
   './Straights.Web.js',
   './Straights.Web.wasm',
   './undoStack.js',
+  './favicon/android-chrome-192x192.png',
+  './favicon/android-chrome-512x512.png',
   './favicon/favicon-16x16.png',
   './favicon/favicon-32x32.png',
   './favicon/favicon-48x48.png',
@@ -27,14 +29,20 @@ const urlsToCache = [
   './favicon/maskable_icon_x192.png',
   './favicon/maskable_icon_x512.png',
   './site.webmanifest',
-  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap'
+  'https://fonts.googleapis.com/css2?family=Nunito:wght@400;600&display=swap',
+  'https://fonts.gstatic.com/s/nunito/v31/XRXV3I6Li01BKofINeaB.woff2',
+  'https://fonts.gstatic.com/s/nunito/v31/XRXV3I6Li01BKofINeaBTMnFcQ.woff2'
 ];
 
 async function fetchFresh(url) {
-  return fetch(url, { cache: 'reload' });
+  console.debug("Fetching ", url)
+  result = await fetch(url, { cache: 'no-store' })
+  console.debug("Response:", result.status, result.statusText)
+  return result
 }
 
 self.addEventListener('install', event => {
+  console.info("Installing service worker", CACHE_NAME)
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
       return Promise.all(
@@ -56,10 +64,11 @@ self.addEventListener('fetch', event => {
       }
 
       // Only cache http(s) requests
+      console.debug("Not found in cache, fetching from network: ", event.request.url)
       if (event.request.url.startsWith('http://') || event.request.url.startsWith('https://')) {
         return fetch(event.request).then(networkResponse => {
           return caches.open(CACHE_NAME).then(cache => {
-            cache.put(event.request, networkResponse.clone());
+            cache.put(event.request, networkResponse.clone())
             return networkResponse;
           });
         });
@@ -71,13 +80,15 @@ self.addEventListener('fetch', event => {
 });
 
 self.addEventListener('activate', event => {
-  const cacheWhitelist = [CACHE_NAME];
+  const cacheWhitelist = [CACHE_NAME]
+  console.info("Activating service worker", CACHE_NAME)
   event.waitUntil(
     caches.keys().then(cacheNames => {
       return Promise.all(
         cacheNames.map(cacheName => {
           if (!cacheWhitelist.includes(cacheName)) {
-            return caches.delete(cacheName);
+            console.info("Deleting old cache", cacheName)
+            return caches.delete(cacheName)
           }
         })
       );
