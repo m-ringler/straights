c5515128a0023b76abb81752481ea6b8e1dca6f8
m-ringler <42344272+m-ringler@users.noreply.github.com>
2026-01-01 21:47:42 +0100

    Fix spdx in gameRenderer.js

--- webdev/ts/shared/gameRenderer.ts
+++ webdev/ts/shared/gameRenderer.ts
@@ -1,6 +1,6 @@
-// SPDX-FileCopyrightText: 2025 Moritz Ringler
+// SPDX-FileCopyrightText: 2025-2026 Moritz Ringler
 //
-// SPDX-License-Identifier: GPL-3.0-or-later';
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 import * as Str8ts from './game.js';
 
